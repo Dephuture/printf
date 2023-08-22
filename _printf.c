7aaae02582e07c371c46b001337d916b4b20f1c7
Dephuture <tenkoranhbright@gmail.com>
2023-08-22 10:09:56 -0700

    betty

--- _printf.c
+++ _printf.c
@@ -1,57 +1,66 @@
 #include "main.h"
-
-int _printf(const char *format, ...){
+/**
+ * _printf - functiion to clone the standard printf_function
+ * @format: takes a variadic input
+ * Return: return (-1) for failure
+ */
+int _printf(const char *format, ...)
+{
 	int printchar = 0;
-
 	va_list arg_list;
-	if (format == NULL){
+
+	if (format == NULL)
+	{
 		return (-1);
 	}
-
 	va_start(arg_list, format);
-
-	while(*format)
+	while (*format)
 	{
-		if (*format != '%'){
+		if (*format != '%')
+		{
 			write(1, format, 1);
 			printchar++;
 		}
 		else
-		{	
+		{
 			format++;
-			if(*format =='\0'){
+			if (*format == '\0')
+			{
 				break;
 			}
 			switch (*format)
 			{
-				case 'c':{
-				char c = va_arg(arg_list, int);
-				write(1,&c,1);
-				printchar++;
-				break;
-				}
-				case '%':{
-				write(1,format, 1);
-				printchar++;
-				break;
-				}
-				case 's':{
-				char *s = va_arg(arg_list,char*);
-				int strlen = 0;
+				case 'c':
+				{
+					char c = va_arg(arg_list, int);
 
-				while(s[strlen] != '\0')
+					write(1, &c, 1);
+					printchar++;
+					break;
+				}
+				case '%':
+				{
+					write(1, format, 1);
+					printchar++;
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(arg_list, char*);
+
+					int strlen = 0;
+
+					while (s[strlen] != '\0')
 					strlen++;
-
-				write (1, s, strlen);
-				printchar += strlen;
-
-				break;
+					write(1, s, strlen);
+					printchar += strlen;
+					break;
 				}
 			}
 		}
 		format++;
 	}
 	va_end(arg_list);
-	return printchar;
+	return (printchar);
 }
 
